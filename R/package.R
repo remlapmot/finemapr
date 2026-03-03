76fb3d7139bba486a061653d1dbcec40f91c73fa
Tom Palmer <remlapmot@hotmail.com>
2026-03-03 12:53:48 +0000

    Add missing importFroms

--- R/package.R
+++ R/package.R
@@ -8,11 +8,11 @@
 #' @importFrom utils head write.table
 #'
 #' @importFrom tibble data_frame as_data_frame 
-#' @importFrom dplyr mutate select select_ arrange filter everything left_join
+#' @importFrom dplyr mutate select select_ arrange filter everything left_join n desc bind_cols
 #' @importFrom magrittr %>% %$%
 #' @importFrom readr read_delim write_delim write_lines read_lines read_tsv cols write_file write_tsv
 #'
-#' @importFrom Matrix Matrix
+#' @importFrom Matrix Matrix sparseMatrix drop0
 #' @import ggplot2
 #' @import ggrepel
 #' @importFrom cowplot plot_grid
